e21370eb8f84fffe656e161e13ce8f73b94b66f1
Gal Cohen <galcohen08@gmail.com>
2018-04-02 14:57:25 +0200

    inputFile
    read file from console

--- src/main/java/Nafora/com/assignment/Assignment/SodokuSolver.java
+++ src/main/java/Nafora/com/assignment/Assignment/SodokuSolver.java
@@ -1,14 +1,22 @@
 package Nafora.com.assignment.Assignment;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 /**
  * Hello world!
  *
  */
 public class SodokuSolver 
 {
-    public static void main( String[] args )
+	private static BufferedReader br;
+    public static void main( String[] argv ) throws Exception
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+    	System.out.println(argv[0]);
+    	String dataFilePath = argv[0];
+    	br = new BufferedReader(new FileReader(dataFilePath));
+        String newLine = br.readLine();
+        while((newLine = br.readLine()) !=null){
+        	System.out.println(newLine);
+        }   	
     }
 }
