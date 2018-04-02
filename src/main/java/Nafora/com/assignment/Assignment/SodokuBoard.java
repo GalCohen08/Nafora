c42516fee566bd680461328b90ac7383c668cb54
Gal Cohen <galcohen08@gmail.com>
2018-04-02 19:58:05 +0200

    Creating The logic

--- src/main/java/Nafora/com/assignment/Assignment/SodokuBoard.java
+++ src/main/java/Nafora/com/assignment/Assignment/SodokuBoard.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class SodokuBoard {
 
-	private int[][] board;
-
+	private static int[][] board;
+	private int size;
 	public SodokuBoard(int size){
-		this.board = new int[size][size];
+		SodokuBoard.board = new int[size][size];
+		this.size = size;
 	}
 	
 	void SetBoardRow(int row, int[] array){
@@ -20,6 +21,54 @@ public class SodokuBoard {
 	    	   System.out.println("\n");
 	        }
 	}
+	
+	public int[][] getSodokuBoard(){
+		return board;
+	}
+	
+	public boolean solveSodoku(int i, int j){
+        if (i == size){
+        	i = 0;
+            if (++j == 9){
+                return true;
+            }
+        }
+        if (board[i][j] != 0){
+            return solveSodoku(i+1,j);
+        }
+        for (int val = 1; val <= 9; ++val) {
+            if (isLegal(i,j,val)) {
+            	board[i][j] = val;
+                if (solveSodoku(i+1,j))
+                    return true;
+            }
+        }
+        board[i][j] = 0; 
+        return false;
+	}
+	
+	 static boolean isLegal(int row, int col, int val){
+		 for (int i = 0; i < 9; ++i){
+			 if(val == board[i][col]){
+				 return false;
+			 }
+		 }
+		 for (int j = 0; j < 9; ++j){
+			 if(val == board[row][j]){
+				 return false;
+			 }
+		 }
+		 int smallRowOffset = (row / 3)*3;
+	     int smallColOffset = (col / 3)*3;
+	     for (int k = 0; k < 3; ++k){
+	    	 for (int m = 0; m < 3; ++m){
+	             if (val == board[smallRowOffset+k][smallColOffset+m]){
+	            	 return false;
+	             }
+	    	 }
+	     }
+		return false;
+	 }
 	
 
 }
