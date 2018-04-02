d2da2af0df6b441a31f1704b43ddadebc73bb41e
Gal Cohen <galcohen08@gmail.com>
2018-04-02 03:09:50 +0200

    First Commit

---
+++ src/main/java/Nafora/com/assignment/Assignment/App.java
@@ -0,0 +1,13 @@
+package Nafora.com.assignment.Assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
